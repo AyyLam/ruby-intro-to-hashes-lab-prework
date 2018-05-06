36ae4eee618a50bd70613db1870635d029755953
Anthony Lam <al3101@nyu.edu>
2018-05-06 23:02:34 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ def monopoly_with_second_tier
 	monopoly = {}
   monopoly[:railroads] = {} 
   monopoly[:railroads][:pieces] = 4 
-  monopoly[:names]
+  monopoly[:railroads][:names] = {} 
 end
 
 def monopoly_with_third_tier
